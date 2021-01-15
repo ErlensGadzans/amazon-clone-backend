bcc5804c2d3b52664d870ba39d53852e61617033
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-01-15 15:45:05 +0200

    All methods for reviews are created

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -75,4 +75,96 @@ productsRouter.put("/:id", async (req, res, next) => {
   }
 });
 
+//REVIEWS
+
+productsRouter.get("/:id/reviews", async (req, res, next) => {
+  try {
+    const reviews = await ProductModel.findById(req.params.id);
+    res.status(200).send(reviews);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+productsRouter.get("/:id/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const { reviews } = await ProductModel.findOne(
+      { _id: mongoose.Types.ObjectId(req.params.id) }, //using mangoose to find object by id
+      {
+        reviews: {
+          $elemMatch: { _id: mongoose.Types.ObjectId(req.params.reviewId) },
+        },
+      }
+    );
+    res.status(200).send(reviews);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+productsRouter.post("/:id/reviews", async (req, res, next) => {
+  try {
+    const review = req.body;
+    const newReview = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { reviews: review },
+      },
+      { runValidators: true, new: true }
+    );
+    res.status(201).send({ newReview });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+productsRouter.delete("/:id/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const { reviews } = await ProductModel.findOneAndUpdate(
+      { _id: mongoose.Types.ObjectId(req.params.id) },
+      {
+        $pull: {
+          reviews: { _id: mongoose.Types.ObjectId(req.params.reviewId) },
+        },
+      }
+    );
+    res.status(204).send(reviews);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+productsRouter.put("/:id/reviews/:reviewId", async (req, res, next) => {
+  try {
+    const product = await ProductModel.findOne(
+      { _id: mongoose.Types.ObjectId(req.params.id) },
+      {
+        reviews: {
+          $elemMatch: { _id: mongoose.Types.ObjectId(req.params.reviewId) },
+        },
+      }
+    );
+
+    const reviewIndex = product.reviews.findIndex(
+      (review) => review._id.toString() === req.params.reviewId
+    );
+
+    const reviewInArray = product.reviews[reviewIndex];
+
+    const updatedReview = { ...reviewInArray._doc, ...req.body };
+
+    product.reviews[reviewIndex] = updatedReview;
+
+    await product.update({ reviews: product.reviews });
+    res.status(204).send(product);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 module.exports = productsRouter;
